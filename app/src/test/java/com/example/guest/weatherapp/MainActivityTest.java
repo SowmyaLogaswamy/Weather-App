cad45ad140a10d41bdb0963de96e83a1fd7ae490
Michael Dunlap and Sowmya Logaswamy <michael+sowmya@>
2017-09-12 16:52:02 -0700

    Test the text field and button and test intent from main activity to forecast activity

---
+++ app/src/test/java/com/example/guest/weatherapp/MainActivityTest.java
@@ -0,0 +1,44 @@
+package com.example.guest.weatherapp;
+
+import android.content.Intent;
+import android.os.Build;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
+
+import static junit.framework.Assert.assertTrue;
+
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+
+public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextViewContent() {
+        TextView weatherMainTextView = (TextView) activity.findViewById(R.id.weatherMainTextView);
+        assertTrue("Weather App".equals(weatherMainTextView.getText().toString()));
+    }
+
+    @Test
+    public void secondActivityStarted() {
+        activity.findViewById(R.id.getForecastButton).performClick();
+        Intent expectedIntent = new Intent(activity, ForecastActivity.class);
+        ShadowActivity shadowActivity = org.robolectric.Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
+    }
+
+}
+
